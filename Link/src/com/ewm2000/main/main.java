75b995a5812f643803b05c181fedce64ca8d7736
Gator <gatortech12@gmail.com>
2016-09-14 20:19:22 -0400

    Commit 1 A start

---
+++ Link/src/com/ewm2000/main/main.java
@@ -0,0 +1,15 @@
+package com.ewm2000.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class main extends JavaPlugin{
+
+	@Override
+	public void onEnable(){
+		
+	}
+	@Override
+	public void onDisable(){
+		
+	}
+}
